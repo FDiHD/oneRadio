54986e71ed8166a79981da8c27061d51b9fe6ddd
Cristea Stelica <ghostrayder9321@gmail.com>
2018-07-04 16:08:30 +0300

    cleaned code

--- app/src/main/java/com/example/fdihd/oneradio/MainActivity.java
+++ app/src/main/java/com/example/fdihd/oneradio/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.fdihd.oneradio;
+
 import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
@@ -12,6 +13,7 @@ import android.widget.ListView;
 
 import android.os.AsyncTask;
 import android.widget.TextView;
+
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
@@ -20,48 +22,46 @@ import org.jsoup.nodes.Document;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
-    String[] nameArray = {"Radio Liberty","Radio Romania Actualitati","Traditional Radio","Radio Lipova","HITRADIO CENTER"};
-    String stream = "http://live.radiotequila.ro:7000/7.html";
+    String[] nameArray = {
+            "Radio Liberty",
+            "Radio Romania Actualitati",
+            "Traditional Radio",
+            "Radio Lipova",
+            "HITRADIO CENTER"};
     String[] infoArray = {
             "http://asculta.radioliberty.ro:1989/7.html",
             "http://89.238.227.6:8002/7.html",
             "http://radiotraditional.zapto.org:7600/7.html",
             "http://radiolipova.radiolipova.net:1717/7.html",
-            "http://212.30.80.195:8000/7.html"
-
-
-    };
-    String[] streamurl= {"http://asculta.radioliberty.ro:1989/","http://89.238.227.6:8002/","http://radiotraditional.zapto.org:7600/","http://radiolipova.radiolipova.net:1717/","http://212.30.80.195:8000/"};
-    private boolean prepared = true;
+            "http://212.30.80.195:8000/7.html"};
+    String[] streamURL = {"http://asculta.radioliberty.ro:1989/",
+            "http://89.238.227.6:8002/",
+            "http://radiotraditional.zapto.org:7600/",
+            "http://radiolipova.radiolipova.net:1717/",
+            "http://212.30.80.195:8000/"};
+    private boolean mPrepared = true;
     ListView listView;
-    TextView scroll;
+    TextView mScroll;
 
     private void generateRandom() {
         new Thread(new Runnable() {
 
             @Override
             public void run() {
-                while (prepared) {
+                while (mPrepared) {
                     Document finalDoc = null;
-                    final String[] arayf = new String[5];
+                    final String[] metaArray = new String[5];
                     try {
-                        for (int i=0; i < infoArray.length;i++) {
+                        for (int i = 0; i < infoArray.length; i++) {
                             finalDoc = Jsoup.connect(infoArray[i])
                                     .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
                                     .referrer("http://www.google.com")
                                     .get();
-
-
                             String x2 = StringUtils.substringBetween(finalDoc.toString(), "28,", "</");
-                            Log.d("STATION", "ID " +i + " output "+ x2);
-                            arayf[i]=x2;
-                            Log.d("STATION", "ID " +i + " output "+ arayf[i]);
-
-
+                            Log.d("STATION", "ID " + i + " output " + x2);
+                            metaArray[i] = x2;
+                            Log.d("STATION", "ID " + i + " output " + metaArray[i]);
                         }
-
-
                     } catch (NullPointerException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
@@ -71,18 +71,13 @@ public class MainActivity extends AppCompatActivity {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-
                     runOnUiThread(new Runnable() {
                         @RequiresApi(api = Build.VERSION_CODES.M)
                         @Override
                         public void run() {
-
-                            CustomListAdapter whatever = new CustomListAdapter(MainActivity.this, nameArray, arayf);
-
-                            listView =  findViewById(R.id.listviewID);
-                            listView.setAdapter(whatever);
-
-
+                            CustomListAdapter listSet = new CustomListAdapter(MainActivity.this, nameArray, metaArray);
+                            listView = findViewById(R.id.listviewID);
+                            listView.setAdapter(listSet);
                         }
                     });
                     try {
@@ -93,57 +88,39 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
-
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         CustomListAdapter whatever = new CustomListAdapter(this, nameArray, infoArray);
-        scroll = findViewById(R.id.textView);
-        listView =  findViewById(R.id.listviewID);
+        mScroll = findViewById(R.id.textView);
+        listView = findViewById(R.id.listviewID);
         listView.setAdapter(whatever);
-
-
-
-        new PlayerTask().execute(stream);
-
-
-
-        listView.setOnItemClickListener(new OnItemClickListener() {
+                listView.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
                                     long id) {
                 Intent intent = new Intent(MainActivity.this, RadioActivity.class);
-                String message = streamurl[position];
-                String message2 =infoArray[position];
-                String message3= nameArray[position];
+                String message = streamURL [position];
+                String message2 = infoArray[position];
+                String message3 = nameArray[position];
                 intent.putExtra("URL", message);
-                intent.putExtra("METADATA",message2);
-                intent.putExtra("NAME",message3);
+                intent.putExtra("METADATA", message2);
+                intent.putExtra("NAME", message3);
                 startActivity(intent);
-
             }
         });
-
     }
-
-
     class PlayerTask extends AsyncTask<String, Void, Boolean> {
-
         @Override
         protected Boolean doInBackground(String... strings) {
-
-          generateRandom();
-            return prepared;
+            generateRandom();
+            return mPrepared;
         }
-
         @Override
         public void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
-
         }
     }
 }
--- app/src/main/java/com/example/fdihd/oneradio/RadioActivity.java
+++ app/src/main/java/com/example/fdihd/oneradio/RadioActivity.java
@@ -1,96 +1,76 @@
 package com.example.fdihd.oneradio;
 
-
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.AudioDeviceInfo;
 import android.media.AudioManager;
-import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.support.annotation.DrawableRes;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.ion.Ion;
-
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 
-public class RadioActivity extends AppCompatActivity {
-    TextView text1;
-    MediaPlayer mediaPlayer;
-    String savedExtra;
-    boolean prepared = false;
-    boolean started = false;
-    private boolean headphones;
-    private String tester2="";
-    boolean h1=true;
-    boolean firststate;
-    boolean sndstate;
-    boolean trdstate;
-    boolean ftstate=true;
-    ImageView i_back;
-    String url;
-    TextView infos;
-    ImageView stp;
-    TextView rinfo;
-    String tinfo;
-    String x2;
-    ImageView image;
-
-
+public final class RadioActivity extends AppCompatActivity {
+    private TextView textLabel;
+    private MediaPlayer mediaPlayer;
+    private String savedExtra;
+    protected boolean mPrepared = false;
+    protected boolean mStarted = false;
+    private boolean headPhones;
+    private String tester2 = "";
+    protected boolean firstState;
+    protected boolean sndState;
+    protected boolean trdState;
+    protected boolean ftState = true;
+    private ImageView imageBack;
+    private String mURL;
+    private TextView mInfos;
+    private ImageView stopPlay;
+    private TextView rInfo;
+    private String tInfo;
+    private String rString;
     @RequiresApi(api = Build.VERSION_CODES.M)
-    private boolean isHeadphonesPlugged(){
-        AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+    private boolean isHeadphonesPlugged() {
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         AudioDeviceInfo[] audioDevices = audioManager.getDevices(AudioManager.GET_DEVICES_ALL);
-        for(AudioDeviceInfo deviceInfo : audioDevices){
-            if(deviceInfo.getType()==AudioDeviceInfo.TYPE_WIRED_HEADPHONES
-                    || deviceInfo.getType()==AudioDeviceInfo.TYPE_WIRED_HEADSET){
+        for (AudioDeviceInfo deviceInfo : audioDevices) {
+            if (deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADPHONES
+                    || deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADSET) {
                 return true;
             }
         }
         return false;
     }
-
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radioactivity);
-        image = findViewById(R.id.imageView);
-        infos= findViewById(R.id.textView2);
-        stp = findViewById(R.id.imageView2);
-        text1 = findViewById(R.id.textView);
-        rinfo =findViewById(R.id.rName);
-        tinfo = getIntent().getStringExtra("NAME");
-        rinfo.setText(tinfo);
-        stp.setImageResource(R.drawable.playh);
-        infos.setText("LOADING");
+        mInfos = findViewById(R.id.textView2);
+        stopPlay = findViewById(R.id.stopPlayImg);
+        textLabel = findViewById(R.id.tLabel);
+        rInfo = findViewById(R.id.rName);
+        tInfo = getIntent().getStringExtra("NAME");
+        rInfo.setText(tInfo);
+        stopPlay.setImageResource(R.drawable.playh);
+        mInfos.setText("LOADING");
         savedExtra = getIntent().getStringExtra("URL");
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         new PlayerTask().execute(savedExtra);
-        stp.setEnabled(false);
-        i_back= findViewById(R.id.i_back);
-        i_back.setOnClickListener(new View.OnClickListener() {
+        stopPlay.setEnabled(false);
+        imageBack= findViewById(R.id.iBack);
+        imageBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
@@ -98,164 +78,124 @@ public class RadioActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-        stp.setOnClickListener(new View.OnClickListener() {
+        stopPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-
-                if (started) {
-                    started = false;
+                if (mStarted) {
+                    mStarted = false;
                     mediaPlayer.pause();
-                    stp.setImageResource(R.drawable.play);
-
+                    stopPlay.setImageResource(R.drawable.play);
                 } else {
-                    started = true;
+                    mStarted = true;
                     mediaPlayer.start();
-                    stp.setImageResource(R.drawable.pause);
-
+                    stopPlay.setImageResource(R.drawable.pause);
                 }
             }
         });
-
-
     }
-
-
     class PlayerTask extends AsyncTask<String, Void, Boolean> {
-
         @Override
         protected Boolean doInBackground(String... strings) {
-
             try {
                 mediaPlayer.setDataSource(strings[0]);
                 mediaPlayer.prepare();
-                prepared = true;
+                mPrepared = true;
                 generateRandom();
-
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            return prepared;
+            return mPrepared;
         }
-
         @Override
         public void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
-            stp.setEnabled(true);
-            infos.setText("");
-            stp.setImageResource(R.drawable.play);
-
+            stopPlay.setEnabled(true);
+            mInfos.setText("");
+            stopPlay.setImageResource(R.drawable.play);
         }
     }
-
     @Override
     public void onPause() {
         super.onPause();
-        if (started) {
+        if (mStarted) {
             mediaPlayer.pause();
         }
     }
-
     @Override
     public void onResume() {
         super.onResume();
-        if (started) {
+        if (mStarted) {
             mediaPlayer.start();
         }
     }
-
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (prepared) {
+        if (mPrepared) {
             mediaPlayer.release();
         }
-        prepared = false;
+        mPrepared = false;
     }
-
     private void generateRandom() {
-        url = getIntent().getStringExtra("METADATA");
+        mURL = getIntent().getStringExtra("METADATA");
         new Thread(new Runnable() {
-
             @Override
             public void run() {
-
-                while (prepared) {
+                while (mPrepared) {
                     Document finalDoc = null;
                     try {
-                                finalDoc = Jsoup.connect(url)
+                        finalDoc = Jsoup.connect(mURL)
                                 .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
                                 .referrer("http://www.google.com")
                                 .get();
-
-
-
-                } catch (NullPointerException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (HttpStatusException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-
-
-
+                    } catch (NullPointerException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (HttpStatusException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                     final Document finalDoc1 = finalDoc;
                     runOnUiThread(new Runnable() {
                         @RequiresApi(api = Build.VERSION_CODES.M)
                         @Override
                         public void run() {
-                            headphones = isHeadphonesPlugged();
-                            if(headphones && h1){
-                                Log.d("HEADPHONES","SUNT BAGATE");
-                                firststate=true;
-                            }
-                            else if (!headphones && !firststate ){
-                                Log.d("HEADPHONES","NU SUNT BAGATE");
-                                sndstate=true;
-
-                            }else if(firststate && sndstate){
-                                Log.d("HEADPHONES","AU FOST BAGATE");
-                                trdstate=true;
-                                h1=false;
-                                if(headphones && trdstate){
-                                    Log.d("HEADPHONES","AU FOST BAGATE, TREBUIE SA CANTE");
-                                    started = true;
-                                    ftstate=true;
+                            headPhones = isHeadphonesPlugged();
+                            if (headPhones) {
+                                Log.d("HEADPHONES STATUS", "ARE PLUGGED IN");
+                                firstState = true;
+                            } else if (!headPhones && !firstState) {
+                                Log.d("HEADPHONES STATUS", "ARE NOT PLUGGED IN");
+                                sndState = true;
+                            } else if (firstState && sndState) {
+                                Log.d("HEADPHONES STATUS", "ARE PLUGGED IN");
+                                trdState = true;
+                                if (headPhones && trdState) {
+                                    Log.d("HEADPHONES STATUS", "ARE PLUGGED IN,PLAY");
+                                    mStarted = true;
+                                    ftState = true;
                                     mediaPlayer.start();
-                                    stp.setImageResource(R.drawable.pause);
-                                }
-                                else if(trdstate && sndstate && ftstate){
-                                    Log.d("HEADPHONES","AU FOST BAGATE, si sunt scoase");
-                                    started = false;
-                                    ftstate=false;
+                                    stopPlay.setImageResource(R.drawable.pause);
+                                } else if (trdState && sndState && ftState) {
+                                    Log.d("HEADPHONES STATUS", "WERE PLUGGED IN, NEW STATE OUT");
+                                    mStarted = false;
+                                    ftState = false;
                                     mediaPlayer.pause();
-                                    stp.setImageResource(R.drawable.play);
-                                }}
-
-
-
-                                x2 = StringUtils.substringBetween(finalDoc1.toString(), "28,", "</");
-
-
-
-                            if (x2 == null) {
-                                Log.d("STATUS ", x2);
-                            }else{
-                                if(x2.equals(tester2)) {
-                                    Log.d("CHANGE STATUS","NOTHING TO CHANGE");
-
-                                }else{
-                                    Log.d("CHANGE STATUS", x2);
-                                    text1.setText(x2);
-                                    tester2 = x2;
+                                    stopPlay.setImageResource(R.drawable.play);
+                                }
+                            }
+                            rString = StringUtils.substringBetween(finalDoc1.toString(), "28,", "</");
+                            if (rString == null) {
+                                Log.d("STATUS ",rString);
+                            } else {
+                                if (rString.equals(tester2)) {
+                                    Log.d("CHANGE STATUS", "NOTHING TO CHANGE");
+                                } else {
+                                    Log.d("CHANGE STATUS", rString);
+                                    textLabel.setText(rString);
+                                    tester2 = rString;
                                 }
                             }
                         }
@@ -268,8 +208,5 @@ public class RadioActivity extends AppCompatActivity {
                 }
             }
         }).start();
-
     }
-
-
 }
